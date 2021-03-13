0d6d3b1a72643c2956c7d7a5271f4b1030bea392
DelDizzle <lukas.delzotto@gmail.com>
2021-03-12 19:37:18 -0500

    Portfolio v0.4
    -updated navbar
    
    -added hero
    
    -added footer
    
    -added links to my GitHub and LinkedIn with logos
    
    -added GNU General Public License

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,20 +1,20 @@
 /**
  * hamburger.js
  * Lukas Del Zotto
+ * 03/10/21 - 03/12/21
  * 
  * controls functionality of the navigation hamburger
  */
-
 function toggleHamburgerMenu(){
-    //get navigation burger element
-    const burger = document.getElementById("navbarBurger");
+    //get navigation hamburger element
+    const hamburger = document.getElementById("navbarBurger");
 
     //get navigation menu element
     const menu = document.getElementById("navbarMenu");
 
-    //toggle if burger is active
-    burger.classList.toggle('is-active');
+    //toggle hamburger is active
+    hamburger.classList.toggle('is-active');
 
-    //toggle if menu is active
+    //toggle menu is active
     menu.classList.toggle('is-active');
 }
